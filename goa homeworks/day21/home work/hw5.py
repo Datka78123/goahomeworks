758d9403f7675d5351f678e41619e7e8d9fea9f3
Datka78123 <ianedale123321@gmail.com>
2025-04-30 18:03:12 +0400

    second commit

--- goa homeworks/day21/home work/hw5.py
+++ goa homeworks/day21/home work/hw5.py
@@ -1,8 +1,6 @@
-#https://www.codewars.com/kata/55d277882e139d0b6000005d/train/python
+#https://www.codewars.com/kata/55d277882e139d0b6000005d/train/pythonq
 
 def find_average(nums):
     count = len(nums)
     total = sum(nums)
     return total / count
-
-
